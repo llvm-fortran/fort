d786a3ef881997e64f58780effe7153ccc457881
arphaman <arphaman@gmail.com>
2013-07-23 19:09:06 +0100

    expanded assign and do implicit decl tests

--- test/Parser/assignedGotoImplicitDecl.f95
+++ test/Parser/assignedGotoImplicitDecl.f95
@@ -1,7 +1,13 @@
-! RUN: %flang -fsyntax-only < %s
+! RUN: %flang -fsyntax-only -verify < %s
 PROGRAM gototest
 
 10  CONTINUE
     ASSIGN 10 TO I
 
 END PROGRAM
+
+subroutine foo
+    implicit none
+10  CONTINUE
+    ASSIGN 10 TO I ! expected-error {{use of undeclared identifier 'i'}}
+end
