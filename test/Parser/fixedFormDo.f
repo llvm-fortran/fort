5c42945b833399be85ab165afa204aeb10107dea
arphaman <arphaman@gmail.com>
2013-08-03 13:53:54 +0100

    added failing fixed-form test

---
+++ test/Parser/fixedFormDo.f
@@ -0,0 +1,13 @@
+C RUN: %flang -fsyntax-only %s
+C RUN: %flang -fsyntax-only -ast-print %s 2>&1 | %file_check %s
+       PROGRAM test
+       INTEGERDONE
+       DOI=1,10
+       ENDDO
+C CHECK: done = 1
+       DONE=1
+C CHECK: done = (1+2)
+       DONE=1+2
+       DODONE=1,10
+       ENDDO
+       E ND PRO GRAMt e s t
