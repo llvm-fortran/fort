faa2e996aa2131d9b89c8432410ed42aa8d952d0
arphaman <arphaman@gmail.com>
2013-09-02 20:09:44 +0100

    improved equivalence test

--- test/CodeGenInAction/equivalence.f95
+++ test/CodeGenInAction/equivalence.f95
@@ -8,6 +8,10 @@ program eqtest
   integer is, i_mat(3,3), i_mat2(3,3)
   equivalence (is, i_mat), (i_mat, i_mat2(2,1))
 
+  real c_elements(2)
+  complex c
+  equivalence (c, c_elements(1))
+
   print *, 'START' ! CHECK: START
   i = 42
   print *, i ! CHECK-NEXT: 42
@@ -18,6 +22,7 @@ program eqtest
 
   x = 2.5
   if(x >= 2.5) print *, 'yes' ! CHECK-NEXT: yes
+  if(i /= 0) print *, 'yeah'  ! CHECK-NEXT: yeah
 
   is = 21
   print *, i_mat(1,1)  ! CHECK-NEXT: 21
@@ -25,5 +30,9 @@ program eqtest
   i_mat2(2,1) = -66
   print *, is          ! CHECK-NEXT: -66
   print *, i_mat(1,1)  ! CHECK-NEXT: -66
+
+  c = (1.0 , 2.0)
+  print *, int(c_elements(1)) ! CHECK-NEXT: 1
+  print *, int(c_elements(2)) ! CHECK-NEXT: 2
 
 end
