7dd12ef0c2ccf8d1ad5ed3b022fc725d1e5c8118
Petr Penzin <penzin.dev@gmail.com>
2018-02-02 16:51:55 -0800

    Modules: don't emit PARAMETER variables, enhance testing
    Stop emitting module variables declared `parameter`.
    
    Check that param module varaible is not emitted, but its value is
    emitted at use site. Make sure that mode variable read works (writing is
    already checked).
    
    For #3, module support.

--- test/CodeGen/module.f95
+++ test/CodeGen/module.f95
@@ -2,14 +2,18 @@
 module mod
   integer N ! CHECK: @__mod_MOD_n_ = internal global i32
   integer c ! CHECK: @__mod_MOD_c_ = internal global i32
+  integer m
+  parameter(m = 10) ! CHECK-NOT: @__mod_MOD_m_
   contains
   subroutine sub(x) ! CHECK: define void @__mod_MOD_sub_(i32* noalias %x)
     integer, intent(out) :: x
-      x = 1 ! CHECK: store i32 1, i32* %x
+      x = m ! CHECK: store i32 10, i32* %x
   end subroutine
   integer function f(x) ! CHECK: define i32 @__mod_MOD_f_(i32* noalias %x)
     integer, intent(in) :: x
       c = x ! CHECK: i32* @__mod_MOD_c
-      f = x + 5
+      ! CHECK: %n = load i32, i32* @__mod_MOD_n_
+      ! CHECK: store i32 %n, i32* %f
+      f = N
   end function
 end module
