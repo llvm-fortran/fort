7b240d6eac41a1bebb67d7be079605582a4758e4
Petr Penzin <penzin.dev@gmail.com>
2018-04-03 09:03:49 -0700

    Add type declaration to module test

--- test/Parser/module.f95
+++ test/Parser/module.f95
@@ -6,6 +6,9 @@ module c   ! CHECK: module c
   contains
 end module ! CHECK: end module
 module mod ! CHECK: module mod
+  type point
+    real :: x, y
+  end type
   ! CHECK: integer n = 10
   ! TODO:  integer, parameter :: n = 10
   integer n
@@ -15,6 +18,12 @@ module mod ! CHECK: module mod
     integer, intent(out) :: x ! CHECK: integer x
       x = 1 ! CHECK: x = 1
   end subroutine ! CHECK: end subroutine
+  function r(a) ! CHECK: real function r(a)
+    implicit none
+    type(point), intent(in) :: a ! CHECK: type point a
+    real :: r
+    r = sqrt(a%x**2 + a%y**2)
+  end function
 end module ! CHECK: end module
 module ! expected-error {{expected identifier after 'module'}}
 end
