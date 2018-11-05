7e66e9f05103fd58df504a80680a26c07b66d2b5
Petr Penzin <penzin.dev@gmail.com>
2018-11-04 17:44:27 -0800

    Modules -- F95 initialization
    Add F95-style parameter initialization to module test.

--- test/Parser/module.f95
+++ test/Parser/module.f95
@@ -10,9 +10,10 @@ module mod ! CHECK: module mod
     real :: x, y
   end type
   ! CHECK: integer n = 10
-  ! TODO:  integer, parameter :: n = 10
-  integer n
-  parameter(n = 10)
+  integer, parameter :: n = 10
+  ! CHECK: integer m = 20
+  integer m
+  parameter(m = 20)
   contains
   subroutine sub(x) ! CHECK: subroutine sub(x)
     integer, intent(out) :: x ! CHECK: integer x
