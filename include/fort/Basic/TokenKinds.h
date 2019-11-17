cb052ca6cb9e05896e538f2350a6a31186387bcb
Petr Penzin <penzin.dev@gmail.com>
2019-11-16 21:28:23 -0800

    TokenKinds: add punctuators
    Add punctuator macro and allow producing better diagnostics for punctuators.
    
    Closes #41

--- include/fort/Basic/TokenKinds.h
+++ include/fort/Basic/TokenKinds.h
@@ -14,12 +14,14 @@
 #ifndef LLVM_FORT_TOKENKINDS_H__
 #define LLVM_FORT_TOKENKINDS_H__
 
+#include "llvm/Support/Compiler.h"
+
 namespace fort {
 namespace tok {
 
 /// TokenKind - This provides a simple uniform namespace for tokens from all
 /// Fortran languages.
-enum TokenKind {
+enum TokenKind : unsigned short {
 #define TOK(X) X,
 #include "fort/Basic/TokenKinds.def"
   NUM_TOKENS
@@ -30,6 +32,19 @@ enum TokenKind {
 /// The name of a token will be an internal name (such as "l_paren") and should
 /// not be used as part of diagnostic messages.
 const char *getTokenName(enum TokenKind Kind);
+
+/// Determines the spelling of simple punctuation tokens like
+/// '!' or '%', and returns NULL for literal and annotation tokens.
+///
+/// This routine only retrieves the "simple" spelling of the token,
+/// and will not produce any alternative spellings (e.g., a
+/// digraph). For the actual spelling of a given Token, use
+/// Preprocessor::getSpelling().
+const char *getPunctuatorSpelling(TokenKind Kind) LLVM_READNONE;
+
+/// Determines the spelling of simple keyword and contextual keyword
+/// tokens like 'int' and 'dynamic_cast'. Returns NULL for other token kinds.
+const char *getKeywordSpelling(TokenKind Kind) LLVM_READNONE;
 
 /// \brief Determines the spelling of simple punctuation tokens like '**' or
 /// '(', and returns NULL for literal and annotation tokens.
